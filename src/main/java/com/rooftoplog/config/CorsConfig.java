238df3d96745a022d8416227978a5b3506d511c8
RainyRooftopCoder <rainyrooftopcoder@gmail.com>
2025-03-22 00:51:00 +0900

    set SQLite

---
+++ src/main/java/com/rooftoplog/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.rooftoplog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://local.rooftoplog.com:8237")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
